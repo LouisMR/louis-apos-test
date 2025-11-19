89386f03a55d44e59eeeceec1f8f0a713b091794
Tiwis22 <louismr22@gmail.com>
2025-11-18 20:15:01 -0500

    Push accordion
    Add the bootstrap SCSS and Javascript.
    Next step, add custom content and titles.

--- modules/asset/ui/src/index.js
+++ modules/asset/ui/src/index.js
@@ -1,5 +1,11 @@
+import 'bootstrap'
 export default () => {
   // Your own project level JS may go here
   // eslint-disable-next-line no-console
   console.log('Hello World');
+
 };
+
+    const myCollapseElement = document.getElementById('collapseOne');
+    const bsCollapse = new bootstrap.Collapse(myCollapseElement, { toggle: false }); // toggle: false prevents automatic toggling on creation
+    bsCollapse.toggle();
