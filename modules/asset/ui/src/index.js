ec07f6742e00d923da79bdbd7fc4c2f0cbddcad6
Tiwis22 <louismr22@gmail.com>
2025-11-18 20:20:52 -0500

    Removed useless js

--- modules/asset/ui/src/index.js
+++ modules/asset/ui/src/index.js
@@ -2,10 +2,4 @@ import 'bootstrap'
 export default () => {
   // Your own project level JS may go here
   // eslint-disable-next-line no-console
-  console.log('Hello World');
-
 };
-
-    const myCollapseElement = document.getElementById('collapseOne');
-    const bsCollapse = new bootstrap.Collapse(myCollapseElement, { toggle: false }); // toggle: false prevents automatic toggling on creation
-    bsCollapse.toggle();
